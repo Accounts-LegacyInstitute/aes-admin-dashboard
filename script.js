9e0382d1970a1d73ed03b29b2bacc12efe762aee
Accounts@LegacyInstitute <acc.legacyinstitute@gmail.com>
2026-06-21 15:14:19 +0530

    Updated Apps Script URL

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Configuration
 const GOOGLE_CLIENT_ID = '137477957854-prdi3poibskfgdi8kdcg2l2sae54e25b.apps.googleusercontent.com';
 const REDIRECT_URI = window.location.origin + window.location.pathname;
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzYH9WZN_2z6w_xHD3Pc_KZWKQzuRCylVnPfDZV5NG2g6dplI1l0RQe5myGJeSxENwn/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyOJy1cpAbohtulpOJytz2YbtwTuqW01f5jccdBTCITWX-REEmeTJqwaNosKTzphJaR/exec';
 const ADMIN_EMAIL = 'acc.legacyinstitute@gmail.com';
 
 // State
